47d569d7b4368247655b0e221563f25b8dcd4850
vinzmc <maurice.marvin@student.umn.ac.id>
2021-11-20 17:05:37 +0700

    week 12 first commit

---
+++ Week12b_00000033942/app/src/main/java/id/ac/umn/week12b_00000033942/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.week12b_00000033942;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
